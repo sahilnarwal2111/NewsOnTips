fdb4cbbce893f107d11b696fc070592bbf95be91
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-02-18 17:26:29 +0530

    Completed Login Singup

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <div style={{padding: "12px 0px", backgroundColor: "#333", color: "#fff", textAlign: 'center', position: 'fixed', width: '100%', height: '85px', bottom: '0', left: '0', right: '0', zIndex: '999'}}>
+            <div className="text-center">
+                <h5>
+                    NewsOnTips
+                </h5>
+            </div>
+        </div>
+    );
+}
